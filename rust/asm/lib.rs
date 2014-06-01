dacdb5ca39a1035aa585c6be39e30da87a83f31d
Piotr Czarnecki <pioczarn@gmail.com>
2014-06-01 22:22:33 +0200

    asm: update for latest Rust. Strings

--- rust/asm/lib.rs
+++ rust/asm/lib.rs
@@ -20,7 +20,7 @@ use syntax::parse::token;
 use collections::HashMap;
 
 use std::vec::Vec;
-use std::strbuf::StrBuf;
+use std::string::String;
 
 #[macro_registrar]
 pub fn macro_registrar(register: |Name, SyntaxExtension|) {
@@ -39,7 +39,7 @@ enum AsmPiece<'a> {
 }
 
 struct Context<'a, 'b> {
-    asm_str: StrBuf,
+    asm_str: String,
     expr: ast::InlineAsm,
     dummy: Option<Box<MacResult>>,
     ecx: &'a mut ExtCtxt<'b>,
@@ -49,18 +49,18 @@ struct Context<'a, 'b> {
     // TODO try to use HashMap<(uint, ~str), uint>
     args: Vec<(@ast::Expr, Option<@ast::Expr>, Option<uint>)>,
     // as used in the string. (args_idx, ty)
-    arg_outputs: Vec<(uint, ~str)>,
-    arg_inputs: Vec<(uint, ~str)>,
+    arg_outputs: Vec<(uint, String)>,
+    arg_inputs: Vec<(uint, String)>,
 
     // Names of parsed named expressions mapped to their index into args.
-    names: HashMap<~str, uint>,
+    names: HashMap<String, uint>,
 
     // Updated as arguments are consumed or methods are entered
     next_argument: uint,
 }
 
 #[inline]
-fn fetch_idx(arg: &mut Vec<(uint, ~str)>, key: (uint, ~str)) -> uint {
+fn fetch_idx(arg: &mut Vec<(uint, String)>, key: (uint, String)) -> uint {
     match arg.as_slice().position_elem(&key) {
         Some(idx) => idx,
         None => {
@@ -98,7 +98,7 @@ impl<'a, 'b> Context<'a, 'b> {
                         Some(&n) => n,
                         None => {
                             self.ecx.span_err(sp, format!("there is no argument named `{}`",
-                                                            name));
+                                                            name).as_slice());
                             self.dummy = Some(DummyResult::expr(sp));
                             return String("");
                         }
@@ -119,53 +119,6 @@ impl<'a, 'b> Context<'a, 'b> {
         }
     }
 
-    // fn verify_piece(&mut self, p: &AsmPiece) {
-    //     match *p {
-    //         fmt_macros::String(..) => {}
-    //         fmt_macros::Argument(ref arg) => {
-    //             // argument second, if it's an implicit positional parameter
-    //             // it's written second, so it should come after width/precision.
-    //             let pos = match arg.position {
-    //                 fmt_macros::ArgumentNext => {
-    //                     let i = self.next_arg;
-    //                     self.next_arg += 1;
-    //                     Exact(i)
-    //                 }
-    //                 fmt_macros::ArgumentIs(i) => Exact(i),
-    //                 fmt_macros::ArgumentNamed(s) => Named(s.to_strbuf()),
-    //             };
-
-    //             self.verify_arg_type(pos);
-    //         }
-    //         fmt_macros::CurrentArgument => fail!("methods not impl"),
-    //         _ => ()
-    //     }
-    // }
-
-    // fn verify_arg_type(&mut self, arg: Position) {
-    //     match arg {
-    //         Exact(arg) => {
-    //             if self.args.len() <= arg {
-    //                 let msg = format!("invalid reference to argument `{}` (there \
-    //                                 are {} arguments)", arg, self.args.len());
-    //                 self.ecx.span_err(self.fmtsp, msg);
-    //                 return;
-    //             }
-    //         }
-
-    //         Named(name) => {
-    //             let span = match self.names.find(&name) {
-    //                 Some(e) => e.span,
-    //                 None => {
-    //                     let msg = format!("there is no argument named `{}`", name);
-    //                     self.ecx.span_err(self.fmtsp, msg);
-    //                     return;
-    //                 }
-    //             };
-    //         }
-    //     }
-    // }
-
     fn format_pieces<'a>(&mut self, s: &'a InternedString, sp: Span) -> Vec<AsmPiece<'a>> {
         let mut pieces = Vec::new();
         let asm_str = s.get();
@@ -183,7 +136,7 @@ impl<'a, 'b> Context<'a, 'b> {
 
         match parser.errors.shift() {
             Some(error) => {
-                self.ecx.span_err(sp, "invalid format string: " + error.as_slice());
+                self.ecx.span_err(sp, format!("invalid format string: {}", error).as_slice());
                 self.dummy = Some(DummyResult::expr(sp));
             }
             None => {}
@@ -226,7 +179,7 @@ pub fn expand_asm_format(ecx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<
         arg_inputs: Vec::new(),
         names: HashMap::new(),
         next_argument: 0,
-        asm_str: StrBuf::new(),
+        asm_str: String::new(),
         expr: ast::InlineAsm {
             asm: InternedString::new(""),
             asm_str_style: ast::CookedStr,
@@ -264,7 +217,7 @@ pub fn expand_asm_format(ecx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<
     }
 
     let clobbers = format!("~\\{{}\\}", clobs.connect("},~{"));
-    cx.expr.clobbers = token::intern_and_get_ident(clobbers);
+    cx.expr.clobbers = token::intern_and_get_ident(clobbers.as_slice());
 
     let asm_expr = p.parse_expr();
     let (asm_str, _) = match expr_to_str(cx.ecx, asm_expr,
@@ -295,7 +248,8 @@ pub fn expand_asm_format(ecx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<
                     if p.eat(&token::RARROW) {
                         // -> out_expr
                         let out = p.parse_expr();
-                        cx.expr.outputs.push((token::intern_and_get_ident("=" + s.get()), out));
+                        let ident = token::intern_and_get_ident(format!("={}", s).as_slice());
+                        cx.expr.outputs.push((ident, out));
                     }
                 }
                 None => {
@@ -315,7 +269,7 @@ pub fn expand_asm_format(ecx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<
                             cx.ecx.span_err(p.span,
                                         format!("expected ident for named argument, \
                                                 but found `{}`",
-                                                p.this_token_to_str()));
+                                                p.this_token_to_str()).as_slice());
                             return DummyResult::expr(sp);
                         }
                     };
@@ -328,7 +282,8 @@ pub fn expand_asm_format(ecx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<
                         Some(&idx) => match cx.args.get(idx) {
                             &(prev, _, _) => {
                                 cx.ecx.span_err(e.span,
-                                                format!("duplicate argument named `{}`", name));
+                                                format!("duplicate argument named `{}`",
+                                                        name).as_slice());
                                 cx.ecx.parse_sess.span_diagnostic.span_note(prev.span,
                                                                             "previously here");
                                 continue
@@ -376,7 +331,8 @@ pub fn expand_asm_format(ecx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<
                 *t.mut2() = Some(cx.expr.outputs.len());
                 match *t {
                     &(_, Some(out), _) | &(out, _, _) => {
-                        cx.expr.outputs.push((token::intern_and_get_ident("=" + *b), out));
+                        let ident = token::intern_and_get_ident(format!("={}", b).as_slice());
+                        cx.expr.outputs.push((ident, out));
                     }
                 }
             }
@@ -390,10 +346,10 @@ pub fn expand_asm_format(ecx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<
     for &(a, ref b) in cx.arg_inputs.iter() {
         match cx.args.get(a) {
             &(out, _, None) if a < cx.args.len() => {
-                cx.expr.inputs.push((token::intern_and_get_ident(*b), out));
+                cx.expr.inputs.push((token::intern_and_get_ident(b.as_slice()), out));
             }
             &(out, _, Some(idx)) if a < cx.args.len() => {
-                cx.expr.inputs.push((token::intern_and_get_ident(idx.to_str()), out));
+                cx.expr.inputs.push((token::intern_and_get_ident(idx.to_str().as_slice()), out));
             }
             _ => {
                 cx.ecx.span_err(sp, "no such input");
@@ -414,7 +370,7 @@ pub fn expand_asm_format(ecx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<
             String(s) => cx.asm_str.push_str(s),
             Output(i) | Input(i) => {
                 cx.asm_str.push_char('$');
-                cx.asm_str.push_str(i.to_str());
+                cx.asm_str.push_str(i.to_str().as_slice());
             }
         }
     }
