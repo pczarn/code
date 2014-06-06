3f07e21c1a5b66daa1fdbc2f8d13cd18ed93d683
Piotr Czarnecki <pioczarn@gmail.com>
2014-06-06 11:49:30 +0200

    test flatmap_iter ... bench:     15356 ns/iter (+/- 214) test nested_iter  ... bench:      2365 ns/iter (+/- 10) test trivec_iter  ... bench:      2692 ns/iter (+/- 5)

--- rust/vectrivec/mod.rs
+++ rust/vectrivec/mod.rs
@@ -35,7 +35,7 @@ impl<'a, H, K, V> Iterator<AryIter<'a, H, K, V>> for TriAryIter<'a, H, K, V> {
                     ptr: self.ptr as *mut H,
                     key: self.key as *mut K,
                     val: self.val as *mut V,
-                    end: (self.ptr as *mut H).offset(SIZE as int),
+                    end: next_ptr as *mut V,
                     marker: marker::ContravariantLifetime,
                     marker2: marker::NoCopy
                 };
@@ -52,14 +52,14 @@ struct AryIter<'a, H, K, V> {
     ptr: *mut H,
     key: *mut K,
     val: *mut V,
-    end: *mut H,
+    end: *mut V,
     marker: marker::ContravariantLifetime<'a>,
     marker2: marker::NoCopy
 }
 
 impl<'a, H, K, V> Iterator<(&'a mut H, &'a mut K, &'a mut V)> for AryIter<'a, H, K, V> {
     fn next(&mut self) -> Option<(&'a mut H, &'a mut K, &'a mut V)> {
-        if self.ptr >= self.end { //or eq
+        if self.val >= self.end { //or eq
             None
         } else {
             unsafe {
@@ -116,17 +116,17 @@ impl<'a, H, K, V> Iterator<(&'a mut H, &'a mut K, &'a mut V)> for AryIter<'a, H,
 //     }
 // }
 
-fn mk_iter<'a>(v: &'a mut Vec<TriAry<u64, uint, uint>>) -> TriAryIter<'a, u64, uint, uint> {
+fn mk_iter<'a, H, K, V>(v: &'a mut Vec<TriAry<H, K, V>>) -> TriAryIter<'a, H, K, V> {
     let r = unsafe {
         let ptr = v.as_mut_ptr();
         let len = v.len();
-        let koff = (*ptr).mut1() as *mut [uint, ..8] as uint - ptr as uint;
-        let voff = (*ptr).mut2() as *mut [uint, ..8] as uint - ptr as uint;
+        let koff = (*ptr).mut1() as *mut [K, ..8] as uint - ptr as uint;
+        let voff = (*ptr).mut2() as *mut [V, ..8] as uint - ptr as uint;
         TriAryIter {
             end: ptr.offset(len as int),
             ptr: ptr,
-            key: (ptr as *mut u8).offset(koff as int) as *mut TriAry<u64, uint, uint>,
-            val: (ptr as *mut u8).offset(voff as int) as *mut TriAry<u64, uint, uint>,
+            key: (ptr as *mut u8).offset(koff as int) as *mut TriAry<H, K, V>,
+            val: (ptr as *mut u8).offset(voff as int) as *mut TriAry<H, K, V>,
             marker: marker::ContravariantLifetime,
             marker2: marker::NoCopy
         }
@@ -134,8 +134,9 @@ fn mk_iter<'a>(v: &'a mut Vec<TriAry<u64, uint, uint>>) -> TriAryIter<'a, u64, u
     r
 }
 
-fn mk_trivec() -> Vec<TriAry<u64, uint, uint>> {
-    let mut v: Vec<TriAry<u64, uint, uint>> = Vec::new();
+fn mk_trivec() -> Vec<TriAry<u64, u16, u8>> {
+    // let mut v: Vec<TriAry<u64, u8, u8>> = Vec::new();
+    let mut v = Vec::new();
     // v.reserve_exact(32);
     for i in range(0, 128) {
         unsafe {
@@ -156,17 +157,18 @@ fn nested_iter(b: &mut Bencher) {
 
     let mut n = 0;
     b.iter(|| {
-        let mut iterref = mk_iter(&mut v);
+        {let mut iterref = mk_iter(&mut v);
         for mut x in iterref {
             // println!("{:?}", x);
-            for (h, k, v) in x {
+            for (h, k, val) in x {
                 *h += 1;
-                *k = (*k + *h as uint) | *v;
+                *k = (*k + (*val as u16)) | (*val as u16);
                 // *k += 3;
-                *v += 2;
+                *val += 2;
                 // test::black_box(*k);
             }
-        }
+        }}
+        // println!("{:?}", v.get(0));
     });
     // println!("{:?}", v.get(0));
     test::black_box(&v);
@@ -183,10 +185,38 @@ fn flatmap_iter(b: &mut Bencher) {
         let mut iter = mk_iter(&mut v);
         for (h, k, v) in iter.flat_map(|i| i) {
             *h += 1;
-            *k = (*k + *h as uint) | *v;
+            *k = (*k + *v as u16) | *v as u16;
             *v += 2;
-            // println!("{}", *k);
             // test::black_box(*k);
+        }
+    });
+    // println!("{}", n);
+    // println!("{:?}", v.get(0));
+    test::black_box(&v);
+}
+
+#[bench]
+fn trivec_iter(b: &mut Bencher) {
+    // let (mut v, mut iter) = mk_trivec();
+    let mut v = mk_trivec();
+    let len = v.len() * 8;
+    let (h, k, val) = unsafe {
+        (
+            v.as_mut_ptr() as *mut u64,
+            (v.as_mut_ptr() as *mut u16).offset(len as int),
+            (v.as_mut_ptr() as *mut u8).offset((len * 2) as int),
+        )
+    };
+
+    b.iter(|| {
+        // let mut iter = mk_iter(&mut v);
+        for i in range(0, len) {
+            let i = i as int;
+            unsafe {
+                *h.offset(i) += 1;
+                *k.offset(i) = (*k.offset(i) + (*val.offset(i) as u16)) | (*val.offset(i) as u16);
+                *val.offset(i) += 2;
+            }
         }
     });
     // println!("{}", n);
