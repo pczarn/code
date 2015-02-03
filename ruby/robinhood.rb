0695d9123ad2a4e2ebde730bcab6c819e9252c20
Piotr Czarnecki <pioczarn@gmail.com>
2015-02-03 13:36:49 +0100

    robinhood: updated

--- ruby/robinhood.rb
+++ ruby/robinhood.rb
@@ -1,65 +1,17 @@
 require 'bigdecimal'
 
-class Integer
-  # binomial coefficient: n C k
-  def choose(k)
-    # n!/(n-k)!
-    pTop = (self-k+1 .. self).inject(1, &:*) 
-    # k!
-    pBottom = (2 .. k).inject(1, &:*)
-    pTop / pBottom
-  end
-end
+# class Integer
+#   # binomial coefficient: n C k
+#   def choose(k)
+#     # n!/(n-k)!
+#     pTop = (self-k+1 .. self).inject(1, &:*) 
+#     # k!
+#     pBottom = (2 .. k).inject(1, &:*)
+#     pTop / pBottom
+#   end
+# end
 
 def fact(n) (1..n).inject(1, :*) end
-
-MEMO = {}
-
-def t(k, d, b)
-   if d >= b or d < 0 or k < 0
-      0
-   elsif k == 0
-      1
-   else
-      hsh = (k.to_i << 32) + (d.to_i << 16) + b.to_i
-      if r = MEMO[hsh]
-         return r
-      end
-
-      r = - (0...k).each.lazy.map{|j|
-         k.choose(j) * (((k+d).to_f/b).floor**(k-j)) * t(j,d,b)
-      }.inject(0, :+)
-
-      MEMO[hsh] = r
-   end
-end
-
-def tz(d, b, z)
-   (0..100).each.lazy.map {|k|
-      t(k, d, b) * z**k / fact(k)
-   }.inject(:+)
-end
-
-def fn(b, a, k, n, d, s)
-   (k*b*a)**(b*(n+k)+d-s) / fact(b*(n+k)+d-s)
-end
-
-def psi(n, b, a)
-   e = Math::E
-   ba = b*a;
-   (0...b).map {|s|
-      p = tz(b - 1 - s, b, ba) / ba
-
-      q = (1...100).map {|k|
-         (e**(-k*ba)) * (0...b).map {|d|
-            fn(b, a, k, n, d, s) - fn(b, a, k, n+1, d, s)
-         }.inject(0, :+)
-      }
-
-      # p p, q
-      p * q.inject(0, :+)
-   }.inject(0, :+)
-end
 
 a = 0.95
 # ary = (0..50).map {|n| psi(BigDecimal::new(n), 1, BigDecimal::new(a, 10)) }
@@ -74,29 +26,10 @@ a = 0.95
 
 # (0.4 .. 0.9).step()
 
-t = [1]
-
-# (1...500).each do |k|
-#    p = 1
-#    bn = 1
-#    t.push -t.reverse.each_with_index.map {|t_j, j|
-#        bn = bn * (k - j) / (j + 1) # k.choose(j+1)
-#        p *= k
-#        bn * p * t_j
-#    }.inject(0, :+)
-# end
-
-T = t
-
-def psi_opt2(n, a)
+def psi_opt(n, a)
    e = Math::E
-   # e = BigDecimal::new(Math::E, 10)
 
    p = 1 / a - 1
-
-   # p = T.each_with_index.map {|t_k, k|
-   #    t_k * a**(k-1) / fact(k)
-   # }.inject(0, :+)
 
    q = (1...1500).map {|k|
       pwr = [(k*a).floor, k+n]
@@ -112,32 +45,13 @@ def psi_opt2(n, a)
       r.reverse.inject(1, :*) * e**-(k*a-pwr.min) * (1 - k*a/(n + k + 1))
    }
 
-   # p p, q
    p * q.inject(0, :+)
 end
 
-def psi_opt3(n, a)
-   e = Math::E
-   # e = BigDecimal::new(Math::E, 10)
-
-   p = 1 / a - 1
-   q = (1...100).map {|k|
-      pwr = [(k*a).floor, k+n]
-      (k*a)**(k+n) * (e**(-(k*a))) / fact(k + n) * (
-         1.to_f - (k*a)/(k + n + 1)
-      )
-   }
-   p * q.inject(:+)
-end
-
-def psi_opt(n, a)
+def psi(n, a)
    e = Math::E
 
    p = 1 / a - 1
-
-   # p = (0..20).each.map {|k|
-   #    t(k, 0, 1) * a**(k-1) / fact(k)
-   # }
 
    q = (1...100).map {|k|
       (e**(-k*a)) * (k*a)**(k+n) / fact(k + n) * (
@@ -145,21 +59,26 @@ def psi_opt(n, a)
       )
    }
 
-   # p p, q
    p * q.inject(0, :+)
 end
 
-a = 0.909
+a = 0.75
 
-prob = 100.times.map {|n| psi_opt2(n, a) }
+prob = 100.times.map {|n| psi_opt(n, a) }
+
+acc = [BigDecimal::new(1)]
+prob_gt = prob.each_with_object(acc) {|elem, acc| acc << acc.last - elem }
 
 # puts psi_opt2(10, BigDecimal::new(a))
-puts prob, prob.inject(:+), prob[0..3].inject(:+), prob[0..7].inject(:+)
+puts prob_gt.map{|elem| elem.to_f }
+puts
+puts prob
+puts
+puts prob.inject(BigDecimal::new(0), :+)
+#, prob.inject(:+), prob[0..3].inject(:+), prob[0..7].inject(:+)
 # puts psi_opt(10, 0.6)
 # puts psi(10, 1, 0.95)
 
-# puts T
-# puts t(0, 0, 1), t(1, 0, 1), t(2, 0, 1), t(3, 0, 1), t(100, 0, 1), t(200, 0, 1)
 
 # 10, 0.95 => 0.0293533090357552505431913736193743933647354805008
 #             0.029353307994242694
