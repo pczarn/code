be751b7b0bb7a4f919b55846a4de25f1d8e87e39
Piotr Czarnecki <pioczarn@gmail.com>
2016-07-06 14:05:16 +0200

    RH: more advanced view with gray color

--- visualize/robinhood.js
+++ visualize/robinhood.js
@@ -1,5 +1,20 @@
 "use strict";
 
+function drawArrow(ctx, pt, angle, size=10) {
+  var alx = pt.x - size*Math.cos(angle - Math.PI/6),
+      aly = pt.y - size*Math.sin(angle - Math.PI/6);
+  var arx = pt.x - size*Math.cos(angle + Math.PI/6),
+      ary = pt.y - size*Math.sin(angle + Math.PI/6);
+  var asx = pt.x - size*.7*Math.cos(angle),
+      asy = pt.y - size*.7*Math.sin(angle);
+  ctx.moveTo(pt.x, pt.y);
+  ctx.lineTo(alx, aly);
+  ctx.lineTo(asx, asy);
+  ctx.lineTo(arx, ary);
+  ctx.lineTo(pt.x, pt.y);
+  ctx.fill();
+}
+
 class robinHood {
   constructor(capacity) {
     this.table = Array(capacity);
@@ -80,35 +95,104 @@ class robinHood {
 
 class mapView {
   constructor(map) {
+    this.map = map;
+  }
+
+  update() {
 
   }
 
-  draw(ctx, x=0, y=0) {
+  draw(ctx) {
+    var side = this.side;
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(this.map.capacity * side, 0);
+    ctx.moveTo(0, side);
+    ctx.lineTo(this.map.capacity * side, side);
+    // draw boxes
+    var ary = [];
+    var iter = this.map.iterator();
+    // Start first square
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, side);
+    // Squares closed
+    for(var i=0; i<this.map.capacity; i++) {
+      var next = iter.next();
+      ctx.moveTo((i + 1) * side, 0);
+      ctx.lineTo((i + 1) * side, side);
+      if(!next.done && next.value !== undefined) {
+        ctx.fillText(next.value.text, i * side + side / 2, side / 2);
+        if(next.value.initial !== undefined) {
+          var edge = {from: i, to: next.value.initial};
+          if(edge.to > edge.from) {
+            edge.from += this.map.capacity;
+          }
+          ary.push(edge);
+        }
+      }
+    }
+    var arrowsIn = new Set();
+    for(let edge of ary) {
+      let dst_x;
+      if(edge.to == edge.from) {
+        dst_x = edge.to * side + side / 3;
+      } else {
+        dst_x = edge.to * side + side * 2 / 3;
+      }
+      arrowsIn.add(dst_x);
+    }
+    ary.sort((a, b) => a.from - a.to - (b.from - b.to));
+    ary.sort((a, b) => a.from - b.from);
+    var levels = [];
+    while(ary.length != 0) {
+      var new_ary = [];
+      var current_level = [];
+      for(var idx=0; idx<ary.length; idx++) {
+        var next = ary[idx];
+        var skip_until = next.from;
+        while(idx + 1 < ary.length && ary[idx + 1].to < skip_until) {
+          new_ary.push(ary[idx + 1]);
+          idx += 1;
+        }
+        current_level.push(next);
+      }
+      ary = new_ary;
+      levels.push(current_level);
+    }
+    for(let levelId=0; levelId<levels.length; levelId++) {
+      for(let edge of levels[levelId]) {
+        let y = side + levelId * 10;
+        if(edge.to != edge.from) {
+          y += 10;
+        }
+        let src_x = edge.from * side + side / 2;
+        ctx.moveTo(src_x, side * 4 / 5);
+        ctx.lineTo(src_x, y + side / 5);
+        let dst_x;
+        if(edge.to == edge.from) {
+          dst_x = edge.to * side + side / 3;
+        } else {
+          dst_x = edge.to * side + side * 2 / 3;
+        }
+        ctx.lineTo(dst_x, y + side / 5);
+        ctx.lineTo(dst_x, side * 4 / 5);
+      }
+    }
+    ctx.stroke();
 
+    for(let dst_x of arrowsIn) {
+        ctx.beginPath();
+        drawArrow(ctx, {x: dst_x, y: side * 4 / 5}, Math.PI*3/2, 7);
+    }
   }
 }
 
 function onLoad() {
   var canvas = document.getElementById('visualization');
-  var side = 55;
   var transX = 0, transY = 0;
   var map = new robinHood(16);
-  // var view = new mapView(map);
-
-  function drawArrow(ctx, pt, angle, size=10) {
-    var alx = pt.x - size*Math.cos(angle - Math.PI/6),
-        aly = pt.y - size*Math.sin(angle - Math.PI/6);
-    var arx = pt.x - size*Math.cos(angle + Math.PI/6),
-        ary = pt.y - size*Math.sin(angle + Math.PI/6);
-    var asx = pt.x - size*.7*Math.cos(angle),
-        asy = pt.y - size*.7*Math.sin(angle);
-    ctx.moveTo(pt.x, pt.y);
-    ctx.lineTo(alx, aly);
-    ctx.lineTo(asx, asy);
-    ctx.lineTo(arx, ary);
-    ctx.lineTo(pt.x, pt.y);
-    ctx.fill();
-  }
+  var view = new mapView(map);
+  view.side = 55;
 
   function draw() {
     if(canvas.getContext) {
@@ -121,91 +205,28 @@ function onLoad() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       // drawing code
+      ctx.strokeStyle = "black";
       ctx.fillStyle = "black";
       ctx.font = '12pt Calibri';
       ctx.textAlign = 'center';
 
-      var firstEntry = map.capacity / 2 - Math.floor(canvas.width / 2 / side);
-      transX = -firstEntry * side;
+      var firstEntry = map.capacity / 2 - Math.floor(canvas.width / 2 / view.side);
+      transX = -firstEntry * view.side;
       transY = 30;
       ctx.translate(transX, transY);
+      view.draw(ctx);
 
-      ctx.beginPath();
-      ctx.moveTo(0, 0);
-      ctx.lineTo(map.capacity * side, 0);
-      ctx.moveTo(0, side);
-      ctx.lineTo(map.capacity * side, side);
-      // draw boxes
-      var ary = [];
-      var iter = map.iterator();
-      // Start first square
-      ctx.moveTo(0, 0);
-      ctx.lineTo(0, side);
-      // Squares closed
-      for(var i=0; i<map.capacity; i++) {
-        var next = iter.next();
-        ctx.moveTo((i + 1) * side, 0);
-        ctx.lineTo((i + 1) * side, side);
-        if(!next.done && next.value !== undefined) {
-          ctx.fillText(next.value.text, i * side + side / 2, side / 2);
-          if(next.value.initial !== undefined) {
-            ary.push({from: i, to: next.value.initial});
-          }
-        }
-      }
-      var arrowsIn = new Set();
-      for(let edge of ary) {
-        let dst_x;
-        if(edge.to == edge.from) {
-          dst_x = edge.to * side + side / 3;
-        } else {
-          dst_x = edge.to * side + side * 2 / 3;
-        }
-        arrowsIn.add(dst_x);
-      }
-      ary.sort((a, b) => a.from - a.to - (b.from - b.to));
-      ary.sort((a, b) => a.from - b.from);
-      var levels = [];
-      while(ary.length != 0) {
-        var new_ary = [];
-        var current_level = [];
-        for(var idx=0; idx<ary.length; idx++) {
-          var next = ary[idx];
-          var skip_until = next.from;
-          while(idx + 1 < ary.length && ary[idx + 1].to < skip_until) {
-            new_ary.push(ary[idx + 1]);
-            idx += 1;
-          }
-          current_level.push(next);
-        }
-        ary = new_ary;
-        levels.push(current_level);
-      }
-      for(let levelId=0; levelId<levels.length; levelId++) {
-        for(let edge of levels[levelId]) {
-          let y = side + levelId * 10;
-          if(edge.to != edge.from) {
-            y += 10;
-          }
-          let src_x = edge.from * side + side / 2;
-          ctx.moveTo(src_x, side * 4 / 5);
-          ctx.lineTo(src_x, y + side / 5);
-          let dst_x;
-          if(edge.to == edge.from) {
-            dst_x = edge.to * side + side / 3;
-          } else {
-            dst_x = edge.to * side + side * 2 / 3;
-          }
-          ctx.lineTo(dst_x, y + side / 5);
-          ctx.lineTo(dst_x, side * 4 / 5);
-        }
-      }
-      ctx.stroke();
+      ctx.save();
+      ctx.translate(-map.capacity * view.side, 0);
+      ctx.strokeStyle = "#cccccc";
+      ctx.fillStyle = "#cccccc";
+      view.draw(ctx);
 
-      for(let dst_x of arrowsIn) {
-          ctx.beginPath();
-          drawArrow(ctx, {x: dst_x, y: side * 4 / 5}, Math.PI*3/2, 7);
-      }
+      ctx.restore();
+      ctx.translate(map.capacity * view.side, 0);
+      ctx.strokeStyle = "#cccccc";
+      ctx.fillStyle = "#cccccc";
+      view.draw(ctx);
     }
 
     window.requestAnimationFrame(draw);
@@ -223,14 +244,15 @@ function onLoad() {
     canvas.addEventListener('mouseup',function(event) {
       var pos = getMousePos(canvas, event);
       pos.y -= 15;
-      if(pos.y >= 0 && pos.y < side) {
-        var bucketId = Math.floor(pos.x / side);
+      if(pos.y >= 0 && pos.y <= view.side && pos.x >= 0 && pos.x <= map.capacity * view.side) {
+        var bucketId = Math.floor(pos.x / view.side);
         if(event.button == 0) {
-          var text = "abc" + Math.floor(Math.random() * 100);
+          var text = "el" + Math.floor(Math.random() * 100);
           map.insert(bucketId, text);
         } else {
           map.remove(bucketId);
         }
+        view.update();
       }
     });
     window.requestAnimationFrame(draw);
