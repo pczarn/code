0ba85ce6df36928ad6ec7d1ebc1a69077dfa6882
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-22 13:22:12 +0200

    RobinHood: remove semicolons

--- visualize/src/components/Hello.vue
+++ visualize/src/components/Hello.vue
@@ -28,10 +28,10 @@
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
-import { drawArrow } from 'src/draw'
+import { row } from 'src/draw'
 
-const PADDING_TOP = 60;
-const SIDE_LENGTH = 45;
+const PADDING_TOP = 60
+const SIDE_LENGTH = 45
 
 export default {
   name: "canvas-drawing",
@@ -49,55 +49,55 @@ export default {
     }
   },
   mounted () {
-    window.requestAnimationFrame(this.draw);
+    window.requestAnimationFrame(this.draw)
   },
   computed: {
     edges () {
-      let side = this.side;
-      let ary = [];
-      let map = this.map;
+      let side = this.side
+      let ary = []
+      let map = this.map
       for(var i=0; i<this.capacity; i++) {
-        var next = map.table[i];
+        var next = map.table[i]
         if(next !== undefined) {
           var edge = {
             from: i,
             to: next.pos % this.capacity
-          };
-          if(edge.to > edge.from) {
-            edge.from += this.capacity;
           }
-          ary.push(edge);
+          if(edge.to > edge.from) {
+            edge.from += this.capacity
+          }
+          ary.push(edge)
         }
       }
       // Compute levels
-      let edges = new Map();
+      let edges = new Map()
       for(let i=0; i<ary.length; i++) {
-        var to = ary[i].to;
+        var to = ary[i].to
         if(!edges.has(to)) {
-          edges.set(to, new Set());
+          edges.set(to, new Set())
         }
-        edges.get(to).add(ary[i].from);
+        edges.get(to).add(ary[i].from)
       }
       // Sort edges by key first
-      edges = new Map([...edges.entries()].sort((a, b) => a[0] - b[0]));
-      let processed = [];
-      let levels = [];
+      edges = new Map([...edges.entries()].sort((a, b) => a[0] - b[0]))
+      let processed = []
+      let levels = []
       for(const [nextTo, nextFrom] of edges) {
-        var skip_until = Math.max(...nextFrom.values());
+        var skip_until = Math.max(...nextFrom.values())
         // Find a suitable level
-        var freeLevel = levels.findIndex(level => level <= nextTo);
+        var freeLevel = levels.findIndex(level => level <= nextTo)
         if(freeLevel == -1) {
-          freeLevel = levels.length;
-          levels.push(skip_until);
+          freeLevel = levels.length
+          levels.push(skip_until)
         } else {
-          levels[freeLevel] = skip_until;
+          levels[freeLevel] = skip_until
         }
         // Move the edge to processed
         processed.push({
           to: nextTo,
           from: nextFrom,
           level: freeLevel
-        });
+        })
       }
       return processed
     },
@@ -107,166 +107,166 @@ export default {
     draw () {
       const canvas = this.$refs.canvas
       if(canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        ctx.canvas.width  = window.innerWidth;
-        ctx.canvas.height  = window.innerHeight;
+        var ctx = canvas.getContext('2d')
+        ctx.canvas.width  = window.innerWidth
+        ctx.canvas.height  = window.innerHeight
 
-        ctx.globalCompositeOperation = 'destination-over';
+        ctx.globalCompositeOperation = 'destination-over'
         // clear canvas
-        ctx.clearRect(-this.transX, -this.transY, canvas.width, canvas.height);
+        ctx.clearRect(-this.transX, -this.transY, canvas.width, canvas.height)
 
         // drawing code
-        ctx.strokeStyle = "black";
-        ctx.fillStyle = "black";
-        ctx.font = '12pt Calibri';
-        ctx.textAlign = 'center';
+        ctx.strokeStyle = "black"
+        ctx.fillStyle = "black"
+        ctx.font = '12pt Calibri'
+        ctx.textAlign = 'center'
 
-        const firstEntry = this.capacity / 2 - Math.floor(canvas.width / 2 / this.side);
-        this.transX = -firstEntry * this.side + this.transMoved;
-        this.transY = PADDING_TOP;
-        ctx.translate(this.transX, this.transY);
+        const firstEntry = this.capacity / 2 - Math.floor(canvas.width / 2 / this.side)
+        this.transX = -firstEntry * this.side + this.transMoved
+        this.transY = PADDING_TOP
+        ctx.translate(this.transX, this.transY)
         this.drawBuckets(ctx)
 
-        ctx.save();
-        ctx.translate(-this.map.capacity * this.side, 0);
-        ctx.strokeStyle = "#cccccc";
-        ctx.fillStyle = "#cccccc";
+        ctx.save()
+        ctx.translate(-this.map.capacity * this.side, 0)
+        ctx.strokeStyle = "#cccccc"
+        ctx.fillStyle = "#cccccc"
         this.drawBuckets(ctx)
 
-        ctx.restore();
-        ctx.translate(this.map.capacity * this.side, 0);
-        ctx.strokeStyle = "#cccccc";
-        ctx.fillStyle = "#cccccc";
+        ctx.restore()
+        ctx.translate(this.map.capacity * this.side, 0)
+        ctx.strokeStyle = "#cccccc"
+        ctx.fillStyle = "#cccccc"
         this.drawBuckets(ctx)
       }
 
-      window.requestAnimationFrame(this.draw);
+      window.requestAnimationFrame(this.draw)
     },
     drawBuckets (ctx) {
-      this.drawBoxes(ctx);
+      this.drawBoxes(ctx)
       for(let edge of this.edges) {
         if(edge.to != this.highlight) {
-          this.drawEdgeSet(ctx, edge);
+          this.drawEdgeSet(ctx, edge)
         }
       }
-      ctx.stroke();
+      ctx.stroke()
       var highlightEdge = this.edges.find(edge => edge.to === this.highlight)
       if(highlightEdge !== undefined) {
         // highlighted
-        ctx.save();
-        ctx.strokeStyle = "red";
-        ctx.fillStyle = "red";
-        ctx.beginPath();
-          this.drawEdgeSet(ctx, highlightEdge);
-        ctx.stroke();
-        ctx.restore();
+        ctx.save()
+        ctx.strokeStyle = "red"
+        ctx.fillStyle = "red"
+        ctx.beginPath()
+          this.drawEdgeSet(ctx, highlightEdge)
+        ctx.stroke()
+        ctx.restore()
       }
     },
     drawBoxes(ctx) {
-      var side = this.side;
+      var side = this.side
       // Draw horizontal boundaries
-      ctx.beginPath();
-      ctx.moveTo(0, 0);
-      ctx.lineTo(this.map.capacity * side, 0);
-      ctx.moveTo(0, side);
-      ctx.lineTo(this.map.capacity * side, side);
+      ctx.beginPath()
+      ctx.moveTo(0, 0)
+      ctx.lineTo(this.map.capacity * side, 0)
+      ctx.moveTo(0, side)
+      ctx.lineTo(this.map.capacity * side, side)
       // Draw boxes
       var iter = this.map.iterator()
       // Start first square
-      ctx.moveTo(0, 0);
-      ctx.lineTo(0, side);
+      ctx.moveTo(0, 0)
+      ctx.lineTo(0, side)
       // Draw closed squares
       for(var i=0; i<this.map.capacity; i++) {
-        var next = iter.next();
-        ctx.moveTo((i + 1) * side, 0);
-        ctx.lineTo((i + 1) * side, side);
+        var next = iter.next()
+        ctx.moveTo((i + 1) * side, 0)
+        ctx.lineTo((i + 1) * side, side)
         if(!next.done && next.value !== undefined) {
-          ctx.fillText(next.value.text, i * side + side / 2, side / 2);
+          ctx.fillText(next.value.text, i * side + side / 2, side / 2)
         }
       }
-      ctx.stroke();
+      ctx.stroke()
     },
     drawEdgeSet (ctx, edgeSet) {
-      ctx.beginPath();
-        const side = this.side;
-        const y = side + edgeSet.level * 10;
+      ctx.beginPath()
+        const side = this.side
+        const y = side + edgeSet.level * 10
         let dst_x
         if(edgeSet.from.has(edgeSet.to)) {
           // Displacement of 0 present.
-          dst_x = edgeSet.to * side + side / 3;
+          dst_x = edgeSet.to * side + side / 3
         } else {
           // This must be farther to the right.
-          dst_x = edgeSet.to * side + side * 2 / 3;
+          dst_x = edgeSet.to * side + side * 2 / 3
         }
         for(let fromEntry of edgeSet.from) {
-          let src_x;
+          let src_x
           if(edgeSet.to == fromEntry) {
-            src_x = fromEntry * side + side * 2 / 3;
+            src_x = fromEntry * side + side * 2 / 3
           } else {
-            src_x = fromEntry * side + side / 2;
+            src_x = fromEntry * side + side / 2
           }
-          ctx.moveTo(src_x, side * 4 / 5);
-          ctx.lineTo(src_x, y + side / 5);
-          ctx.lineTo(dst_x, y + side / 5);
-          ctx.lineTo(dst_x, side * 4 / 5);
+          ctx.moveTo(src_x, side * 4 / 5)
+          ctx.lineTo(src_x, y + side / 5)
+          ctx.lineTo(dst_x, y + side / 5)
+          ctx.lineTo(dst_x, side * 4 / 5)
         }
-      ctx.stroke();
-      ctx.beginPath();
-        drawArrow(ctx, {x: dst_x, y: side * 4 / 5}, Math.PI*3/2, 7);
-      ctx.fill();
+      ctx.stroke()
+      ctx.beginPath()
+        row(ctx, {x: dst_x, y: side * 4 / 5}, Math.PI*3/2, 7)
+      ctx.fill()
     },
 
     mousemove (e) {
-      const event = e || event;
-      this.highlight = null;
+      const event = e || event
+      this.highlight = null
       if(this.dragging) {
-        const delta = event.offsetX - this.lastX;
-        this.transMoved += delta;
-        this.lastX = event.offsetX;
+        const delta = event.offsetX - this.lastX
+        this.transMoved += delta
+        this.lastX = event.offsetX
       } else {
         const bucket = this.bucketFromMousePos(event)
         if(bucket !== null) {
-          this.highlight = bucket;
+          this.highlight = bucket
         }
       }
     },
     mousedown (e) {
-      const event = e || event;
-      this.lastX = event.offsetX;
+      const event = e || event
+      this.lastX = event.offsetX
       const bucket = this.bucketFromMousePos(event)
       if(bucket === null) {
         // dragging outside the box
-        this.dragging = true;
+        this.dragging = true
       }
     },
     mouseup (event) {
-      this.dragging = false;
+      this.dragging = false
       const bucket = this.bucketFromMousePos(event)
       if(bucket !== null) {
         if(event.button == 0) {
-          var text = "el" + Math.floor(Math.random() * 100);
-          var randomInt = Math.floor(Math.random() * (1 << 16));
+          var text = "el" + Math.floor(Math.random() * 100)
+          var randomInt = Math.floor(Math.random() * (1 << 16))
           this.insert({
             pos: bucket + randomInt * this.capacity,
             value: text
-          });
+          })
         } else {
-          this.remove(bucket);
+          this.remove(bucket)
         }
       }
     },
     wheel (event) {
       if(event.deltaMode == MouseEvent.DOM_DELTA_PIXEL) {
-        this.transMoved += event.deltaY;
+        this.transMoved += event.deltaY
       } else {
-        this.transMoved += event.deltaY * 35;
+        this.transMoved += event.deltaY * 35
       }
     },
     contextmenu (event) {
       var bucket = this.bucketFromMousePos(event)
       if(bucket) {
-        event.preventDefault();
-        return false;
+        event.preventDefault()
+        return false
       }
     },
 
@@ -288,16 +288,16 @@ export default {
 
     resetMap (event) {
       this.reset()
-      this.transMoved = 0;
+      this.transMoved = 0
     },
     resetX (event) {
-      this.transMoved = 0;
+      this.transMoved = 0
     },
     insertRandom (event) {
       for(var i=0; i<10; i++) {
-        var text = "el" + Math.floor(Math.random() * 100);
-        var randomInt = Math.floor(Math.random() * (1 << 16));
-        this.insert({ pos: randomInt, value: text });
+        var text = "el" + Math.floor(Math.random() * 100)
+        var randomInt = Math.floor(Math.random() * (1 << 16))
+        this.insert({ pos: randomInt, value: text })
       }
     },
     ...mapMutations({
--- visualize/src/draw.js
+++ visualize/src/draw.js
@@ -1,13 +1,13 @@
 export function drawArrow(ctx, pt, angle, size=10) {
   var alx = pt.x - size*Math.cos(angle - Math.PI/6),
-      aly = pt.y - size*Math.sin(angle - Math.PI/6);
+      aly = pt.y - size*Math.sin(angle - Math.PI/6)
   var arx = pt.x - size*Math.cos(angle + Math.PI/6),
-      ary = pt.y - size*Math.sin(angle + Math.PI/6);
+      ary = pt.y - size*Math.sin(angle + Math.PI/6)
   var asx = pt.x - size*.7*Math.cos(angle),
-      asy = pt.y - size*.7*Math.sin(angle);
-  ctx.moveTo(pt.x, pt.y);
-  ctx.lineTo(alx, aly);
-  ctx.lineTo(asx, asy);
-  ctx.lineTo(arx, ary);
-  ctx.lineTo(pt.x, pt.y);
+      asy = pt.y - size*.7*Math.sin(angle)
+  ctx.moveTo(pt.x, pt.y)
+  ctx.lineTo(alx, aly)
+  ctx.lineTo(asx, asy)
+  ctx.lineTo(arx, ary)
+  ctx.lineTo(pt.x, pt.y)
 }
