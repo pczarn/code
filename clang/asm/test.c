ad8a13f9d2aeb6b27d6950ad8949080c3b6985dd
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-30 20:16:03 +0100

    clang test asm

--- clang/asm/test.c
+++ clang/asm/test.c
@@ -1,3 +1,10 @@
+__attribute((noinline, naked)) int NakedTest(int value, int value2)
+{
+	asm("");
+}
+
+asm("BareTest:");
+
 static __attribute__((used)) int var1;
 int f1() {
 	int out;
@@ -22,11 +29,39 @@ int f2(int p1, int p2) {
 	return out;
 }
 
+int *func4a(int *p) {
+	printf("in-out %d\n", *p);
+	return p;
+}
+
 int func4(int parm) {
-	asm ("add $0xff, %0; add %1, %0": "+r" (parm) : "i"(123) : "cc");
+	int *p = &parm;
+	asm ("add $0xff, %0; add %1, %0": "+r" (*func4a(p)) : "i"(123) : "cc");
+	// asm ("add $0xff, %0; add %1, %0": "=r"(*func4a(p)) : "i"(123), "r"(*func4a(p)) : "cc");
 	return parm;
+}
+
+// double in_out_early(double p1)
+// {
+// 	double out = 0.1;
+// 	asm ("fadd %1\n\t"
+// 		: "+&t" (out) : "f" (p1));
+// 	return out;
+// }
+
+int in_out_early(int p1)
+{
+	int out = 7;
+	asm (
+		  "add %1, %0\n\t"
+		  "add %10, %0 \n\t"
+		  "add %2, %0\n\t"
+		: "+&r"(out) : "r"(out), "r"(p1), "r"(out), "r"(out), "r"(out), "r"(out), "r"(out), "r"(out), "r"(out), "r"(out), "r"(out), "r"(out), "r"(out) : "cc");
+	return out;
 }
 
 int main() {
 	f1();
+	func4(1234);
+	in_out_early(0.2);
 }
